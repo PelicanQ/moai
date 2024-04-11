4956cb4bd207473d694e83d957ec30c94d3b200e
PelicanQ <77930606+PelicanQ@users.noreply.github.com>
2024-04-11 18:00:53 +0100

    moai

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Audio } from "expo-av";
 
 export default function App() {
+  const [pressing, setPressing] = useState(false);
+  const [sound, setSound] = useState();
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/moai.mp3"),
+      { volume: 1 }
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+  }
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const pressIn = () => {
+    setPressing(true);
+    playSound().catch((e) => console.log("failed to play sound", e));
+  };
+  const pressOut = () => {
+    setPressing(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPressIn={pressIn} onPressOut={pressOut}>
+        <Text style={[styles.moai, { fontSize: pressing ? 150 : 160 }]}>
+          🗿
+        </Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  moai: {
+    fontSize: 70,
   },
 });
